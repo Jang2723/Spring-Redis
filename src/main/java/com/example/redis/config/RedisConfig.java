88b6e58b3d6934e593734f7f24274befa6c511a8
Jang2723 <jangsb46@gmail.com>
2024-02-13 14:44:27 +0900

    02/13 redis에 세션 저장

--- src/main/java/com/example/redis/config/RedisConfig.java
+++ src/main/java/com/example/redis/config/RedisConfig.java
@@ -9,7 +9,7 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-@EnableRedisHttpSession
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 10) // session을 얼마나 오래 유지할 것이지에 대한 설정
 public class RedisConfig {
     @Bean
     public RedisTemplate<String, PersonDto> personDtoRedisTemplate(
@@ -20,12 +20,16 @@ public class RedisConfig {
         // 주어진 데이터의 직렬화 방식을 결정
         // Redis의 Value는 결국 문자열 형식이니까,
         // 주어진 데이터(DTO)를 어떻게 문자열로 바꿀 것인지
-        // 미리 만들어진 JSON 변환기를 설정
 //        template.setDefaultSerializer(RedisSerializer.json());
         // 미리 만들어진 String 변횐기를 설정
         template.setKeySerializer(RedisSerializer.string());
         // 미리 만들어진 JSON 변환기를 설정
         template.setValueSerializer(RedisSerializer.json());
         return template;
+    }
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerialize() {
+        return RedisSerializer.json();
     }
 }
